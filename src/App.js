e7e13b13d27c198d5ef2e383b8a2d9a290578317
shubham <shubhamchitransh8@gmail.com>
2018-08-11 11:11:27 +0530

    React Native Application
    This a simple application that uses firebase authntication to login and logout the user. It checks if the email is new then it creates the account with the entered email and password and if the email is already registered then it validates it that the entered password is correct or not.

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDxL3q6hrhr4jSWFjZKHmwp0-Wutj2qlNU",
+      authDomain: "authentication-2a19e.firebaseapp.com",
+      databaseURL: "https://authentication-2a19e.firebaseio.com",
+      projectId: "authentication-2a19e",
+      storageBucket: "authentication-2a19e.appspot.com",
+      messagingSenderId: "441784479150"
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View style={{ flexDirection: 'row' }}>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </View>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
+      </View>
+    );
+  }
+}
+
+export default App;
